a901a33fe87a8c77afddb9838a72f38c43e7241e
ARNAUD <parnaud@debian>
2014-04-29 19:20:32 +0200

    create

---
+++ sysAdsBlock.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# http get
+import urllib
+import urllib2
+# regex
+import re
+# date
+import time
+# test file exist
+import os
+# Detect OS
+import platform
+# copy file
+import shutil
+# import ui.py QT4 GUI
+import sys
+
+
+class sysadsblock():
+
+    def __init__(self):
+        self.config = self.configuration(self.detect_os())
+        if self.check_install():
+            print "Install OK"
+        else:
+            if self.installer():
+                print "Installing ..."
+            else:
+                print "installing error"
+                exit()
+    def detect_os(self):
+        """
+        detect os to know correct path, ect.
+        Return array
+        """
+        print "detect_os()"
+        systemEnv = {}
+        systemEnv["sys"] = platform.system()
+        systemEnv["release"] = platform.release()
+        systemEnv["os"] = os.name
+        systemEnv["arch"] = platform.architecture()[0]
+        for el in systemEnv:
+            print el + " = " + systemEnv[el]
+        return systemEnv
+
+    def configuration(self,systemEnv):
+        """
+        Generate personnal config with systemEnv from detect_os
+        Return array
+        """
+        print "config()"
+        config = {}
+        # user path
+        config["userPath"] = os.path.expanduser ("~")
+        # System hosts file
+        if systemEnv["sys"] == "Linux":
+            config["etcHost"]  = "/etc/hosts"
+            config["cmdPing"]  = "ping -c 1 " + PING_SITE
+        elif systemEnv["sys"] == "Windows":
+            config["etcHost"]  = "C:\WINDOWS\system32\drivers\etc\hosts"
+            config["cmdPing"]  = "ping " + PING_SITE
+        else:
+            print "Only linux for the moment ..."
+            showerror('Not implemented', 'Only linux for the moment ...')
+            exit()
+        # hosts file build by the app
+        config["appHosts"]  = config["userPath"]  + os.sep +  ".hosts.pyDenyAll"
+        # backup of user hosts file
+        config["sysBckpHosts"] = config["etcHost"] + ".original"
+        for el in config:
+            print el + " = " + config[el]
+        #exit()
+        return config
+
+    def test_connect(self):
+        """
+        Internet ping,
+        Return Bool
+        """
+        print "test_connect()"
+        if os.system(self.config["cmdPing"]):
+            return False
+        else:
+            return True
+
+    def check_install(self):
+        """
+        Check if user hosts file is backup
+        """
+        print "check_install()"
+        if (os.path.isfile(self.config["sysBckpHosts"]) == True):
+            return True
+        else:
+            return False
+
+    def installer(self):
+        """
+        Create backup files
+        Return Bool
+        """
+        print "installer()"
+        shutil.copyfile(self.config["etcHost"], self.config["sysBckpHosts"])
+        if self.check_install():
+            return True
+        else:
+            return False
+
+    def reinit(self):
+        """
+        Clear old app hosts file, create a new one
+        Return Bool
+        """
+        print "reinit()"
+        if (os.path.isfile(self.config["appHosts"]) == True):
+            os.remove(self.config["appHosts"])
+            return True
+        else:
+            return False
+
+    def fetch_hosts(self,provider):
+        """
+        fetch blacklist hosts file update,
+        Return array
+        """
+        print "fetch_hosts()"
+        print "Downloading updated hosts file"
+        req = urllib2.Request(provider, headers={ 'User-Agent': 'Mozilla/5.0' } )
+        try:
+            headers = { 'User-Agent' : 'Mozilla/5.0' }
+            hostsPage = urllib2.urlopen(req,timeout=100)
+            hostsPage = hostsPage.read()
+            hostsPage = hostsPage.split("\n")
+            return hostsPage
+        except urllib2.URLError, e:
+            print "error"
+            return 0
+        
+
+    def build_app_hosts(self,hostsPage):
+        """
+        Build the hosts file
+        hostsPage is array
+        """
+        print "build_app_hosts()"
+        baseHostsFile = open(self.config["etcHost"], "r")
+        baseHostsFile = baseHostsFile.read()
+        baseHostsFile = baseHostsFile.split("\n")
+        baseHostsArray = []
+        for lineHostsFile in baseHostsFile:
+            if '# - - - S T O P - E D I T - H E R E - - - #' in lineHostsFile:
+                break
+            else:
+                print lineHostsFile
+                baseHostsArray.append(lineHostsFile)   
+        print "break"   
+        hostsFile = open(self.config["appHosts"], "w")
+        print "Writing ..."
+        regexIpLocal = re.compile('^127.0.0.1',re.I)
+        regexComment = re.compile('^#',re.I)
+        for el in baseHostsArray:
+            hostsFile.write(el + "\n")
+        hostsFile.write("# - - - S T O P - E D I T - H E R E - - - #\n")
+        hostsFile.write("# END ORIGINALS ENTRIES\n")
+        hostsFile.write("# ----------------------------------------------------------\n\n")
+        hostsFile.write("# This hosts file was generated by pyDenyAll from " + PROVIDER[DEFAULT_PROVIDER] + "\n")
+        hostsFile.write("# " + time.strftime('%d/%m/%y %H:%M',time.localtime()) + "\n\n")
+        hostsFile.write("# ----------------------------------------------------------\n\n")
+        for ligne in hostsPage:
+            ligne = ligne.strip()
+            matchRegexIpLocal = regexIpLocal.search(ligne)
+            matchRegexComment = regexComment.search(ligne)
+            if matchRegexIpLocal:
+                if ligne not in baseHostsArray:
+                    #ligne = ligne.replace("127.0.0.1","127.0.0.2")
+                    hostsFile.write(ligne + "\n")
+            else:
+                if matchRegexComment:
+                    hostsFile.write(ligne + "\n")
+        hostsFile.write("# ----------------------------------------------------------\n\n")
+        hostsFile.close()
+        shutil.copyfile(self.config["appHosts"], self.config["etcHost"])
+        print "OK"     
+        print self.printCountLines()
+        #self.labelDynamic = wx.StaticText(self.panelButton, 1, self.printCountLines(), style = wx.ALIGN_CENTRE)
+        self.labelDynamic.SetLabel(self.printCountLines())
+        return 0
+
+    def no_ads(self, event):
+        print "no_ads()"
+        if self.test_connect() == False:
+            print "Network error"
+            return 0
+        else:
+            print "Network OK"
+            self.reinit()
+            self.labelDynamic.SetLabel( "Please wait ...")
+            self.txtStatusBar.SetStatusText(u"Dl from " + PROVIDER[DEFAULT_PROVIDER] + " ...")
+            print "Dl from " + PROVIDER[DEFAULT_PROVIDER] + " ..."
+            hostsPage = self.fetch_hosts(PROVIDER[DEFAULT_PROVIDER])
+            if (hostsPage != 0):
+                self.build_app_hosts(hostsPage)
+                self.txtStatusBar.SetStatusText(u"Blacklist added !")
+                print "Done !"
+            else:
+                self.txtStatusBar.SetStatusText(u"Error while dowloading !")
+                print "Error while dowloading !"
+
+    def yes_ads(self, event):
+        print "yes_ads()"
+        self.txtStatusBar.SetStatusText(u"Reinit hosts file ...")
+        baseHostsFile = open(self.config["etcHost"], "r")
+        baseHostsFile = baseHostsFile.read()
+        baseHostsFile = baseHostsFile.split("\n")
+        baseHostsArray = []
+        for lineHostsFile in baseHostsFile:
+            if '# - - - S T O P - E D I T - H E R E - - - #' in lineHostsFile:
+                break
+            else:
+                print lineHostsFile
+                baseHostsArray.append(lineHostsFile)   
+        print "break"   
+        hostsFile = open(self.config["appHosts"], "w")
+        print "Writing ..."
+        regexIpLocal = re.compile('^127.0.0.1',re.I)
+        regexComment = re.compile('^#',re.I)
+        for el in baseHostsArray:
+            if el.replace("\n", "") != "":
+                hostsFile.write(el + "\n")
+        hostsFile.close()
+        shutil.copyfile(self.config["appHosts"], self.config["etcHost"])
+        print self.printCountLines()
+        self.labelDynamic.SetLabel(self.printCountLines())
+        self.txtStatusBar.SetStatusText(u"Hosts restored !")
+        print "Hosts restored !"
+
+    def countLine(nf, fdl='\n', tbuf=16384):
+        """Compte le nombre de lignes du fichier nf"""
+        c = 0
+        f = open(nf, 'rb')
+        while True:
+            buf = None
+            buf = f.read(tbuf)
+            if len(buf)==0:
+                break
+            c += buf.count(fdl)
+        f.seek(-1, 2)
+        car = f.read(1)
+        if car != fdl:
+            c += 1
+        f.close()
+        return c   
+        
